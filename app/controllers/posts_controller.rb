2e06a8837ace9c088e7cc53822f99f4ba0e101dd
Patrick Kim <pkim050@gmail.com>
2019-05-11 11:57:46 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,9 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @users = @post.users.uniq
-
+    @comment = Comment.new
+    @comment.build_user
+    @comment.post = @post
   end
 
   def index
